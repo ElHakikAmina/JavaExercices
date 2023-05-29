32e53104d5e467ddf8cd7c85b0f59861e0cabff5
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-05-29 21:05:23 +0000

    exercices en Java

---
+++ ex1.java
@@ -0,0 +1,18 @@
+package exercicesCorriger;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String p1 = "* * * * * * ==================================\n * * * * *  ==================================";
+		  String p2 = "==============================================";
+		  for (int i = 0; i < 4; i++) {
+		   System.out.println(p1);
+		  }
+		  System.out.println("* * * * * * ==================================");
+		  for (int i = 0; i < 6; i++) {
+		   System.out.println(p2);
+		  }
+	}
+
+}
